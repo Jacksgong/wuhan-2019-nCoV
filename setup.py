fd448ebf500ce40a36988492d5dbb0dbbac5036f
jacks <gongzhenjie@bytedance.com>
2020-01-24 13:52:27 +0800

    ci: upgrade to v0.0.9 for fix dingxiangyuan no json format parse failed issue

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Wuhan2019nCoV',
-    version='0.0.8',
+    version='0.0.9',
     packages=find_packages(exclude=['ats']),
 
     install_requires=['requests'],
